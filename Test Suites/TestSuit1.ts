cf52545fa638bf329d8d548ecd1a5f1fbfed35da
manchikatlavineel <manchikatla2706@gmail.com>
2020-12-21 19:50:25 +0530

    New

--- Test Suites/TestSuit1.ts
+++ Test Suites/TestSuit1.ts
@@ -11,4 +11,10 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>f6f36991-c585-4464-bc0e-74be6a869c49</testSuiteGuid>
+   <testCaseLink>
+      <guid>d3cf9b6f-d1d0-4c64-83bd-9ce7a032ac04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
