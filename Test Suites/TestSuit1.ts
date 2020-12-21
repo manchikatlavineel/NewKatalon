009bcf8ce314c9a11f82e988e366105d3e0388c0
vineel.manchikatla <vineel.manchikatla@Otsi-Lap320.otsi.com>
2020-12-21 19:23:21 +0530

    Adding Project

---
+++ Test Suites/TestSuit1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuit1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f6f36991-c585-4464-bc0e-74be6a869c49</testSuiteGuid>
+</TestSuiteEntity>
